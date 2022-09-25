3f07683e787b708c31235c9c5357fb45b4b9f02d
Johannes Schmidt <johannes.schmidt.vik@gmail.com>
2022-09-25 15:20:10 +0200

    feat: add application in app.py

---
+++ src/example_app/app.py
@@ -0,0 +1,31 @@
+import httpx
+import uvicorn
+from fastapi import FastAPI, HTTPException, Path, status
+from httpx import Response
+
+# app
+app: FastAPI = FastAPI()
+
+
+@app.get(path="/{number}", status_code=status.HTTP_200_OK)
+def get_pokemon(
+        number: int = Path(title="The Pokémon to get (based on number)", ge=1, le=151)
+) -> dict:
+    pokemon_url: str = f"https://pokeapi.co/api/v2/pokemon/{number}"
+
+    try:
+        response: Response = httpx.get(url=pokemon_url)
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Could not send a request to {pokemon_url}",
+        )
+
+    if response.status_code == status.HTTP_200_OK:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail=response.text)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="app:app", host="127.0.0.1", port=9000, reload=True)
